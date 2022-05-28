5a3317c881845a93e51ef5e58915ab29e8da4ffa
Jimenez04 <66088807+Jimenez04@users.noreply.github.com>
2022-05-28 09:38:30 -0600

    Cambios

---
+++ src/ingenieriainvestigacion/EmpleadoBaseMasComision.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ingenieriainvestigacion;
+
+/**
+ *
+ * @author jose0
+ */
+public class EmpleadoBaseMasComision extends EmpleadoPorComision{
+    
+    private double salarioBase;
+    
+    public EmpleadoBaseMasComision(String primerNombre, String apellidoPaterno, String numeroSocial, double ventasBrutas, double tarifaComision, double salarioBase) {
+        super(primerNombre, apellidoPaterno, numeroSocial, ventasBrutas, tarifaComision);
+         if(salarioBase < 0.0){
+                throw new IllegalArgumentException(
+                        "El salarioBase debe ser mayor a >= 0.0"
+                );
+         }
+        this.salarioBase = salarioBase;
+    }
+    
+    @Override
+    public double ingresos() {
+        return getSalarioBase() + super.ingresos();
+    }
+    
+    /**
+     * @return the salarioBase
+     */
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    /**
+     * @param salarioBase the salarioBase to set
+     */
+    public void setSalarioBase(double salarioBase) {
+        if(salarioBase < 0.0 ){
+        throw new IllegalArgumentException(
+                "El salario debe ser mayor >= 0.0"
+        );
+     }
+     this.salarioBase = salarioBase;
+    }
+   
+}
---
+++ src/ingenieriainvestigacion/EmpleadoPorHoras.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ingenieriainvestigacion;
+
+/**
+ *
+ * @author jose0
+ */
+public class EmpleadoPorHoras extends Empleado{
+
+    private double sueldo;
+    private double horas;
+    
+    public EmpleadoPorHoras(String primerNombre, String apellidoPaterno, String numeroSocial, double sueldo, double horas) {
+        super(primerNombre, apellidoPaterno, numeroSocial);
+        
+        if(horas < 0.0){
+                throw new IllegalArgumentException(
+                        "Las horas deben ser mayor a >= 0.0"
+                );
+            }
+        if(sueldo < 0.0){
+                throw new IllegalArgumentException(
+                        "El sueldo por horas debe ser >= 0.0"
+                );
+            }
+        this.horas = horas;
+        this.sueldo = sueldo;
+    }
+
+    
+    @Override
+    public double ingresos() {
+        if (getHoras() <= 40) {
+            return getSueldo() * getHoras();
+        }else {
+            return 40 * getSueldo() + (getHoras() - 40 ) * getSueldo() * 1.5;
+        }
+    }  
+    
+    /**
+     * @return the sueldo
+     */
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    /**
+     * @param sueldo the sueldo to set
+     */
+    public void setSueldo(double sueldo) {
+        if(sueldo < 0.0){
+        throw new IllegalArgumentException(
+                "El sueldo por horas debe ser >= 0.0"
+            );
+         }
+         this.sueldo = sueldo;
+    }
+
+    /**
+     * @return the horas
+     */
+    public double getHoras() {
+        return horas;
+    }
+
+    /**
+     * @param horas the horas to set
+     */
+    public void setHoras(double horas) {
+        if(horas < 0.0 || horas > 168.00){
+        throw new IllegalArgumentException(
+                "Las horas deben ser mayores >= 0.0"
+            );
+         }
+         this.horas = horas;
+    }
+    
+}
